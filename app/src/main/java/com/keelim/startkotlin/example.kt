8eba7c2a179880f7b34ff12d0d7c52d2a712f257
keelim <kimh00335@gmail.com>
2019-07-13 02:17:23 +0900

    2019 07 13'

---
+++ app/src/main/java/com/keelim/startkotlin/example.kt
@@ -0,0 +1,16 @@
+// 2019 07 13
+window.addMouseListener(object: MouserAdapter(){
+    override fun mous~~
+})
+
+open class A(x:Int){
+    public open val y:Int = x
+}
+
+interface B{
+
+}
+
+val ab: A = object :A(1). B{
+    override val y = 15
+}
