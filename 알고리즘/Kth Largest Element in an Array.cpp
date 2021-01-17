b78b005083d887efc44671b4c2e1884cd768e0ba
Keel_im <kimh00335@gmail.com>
2021-01-17 12:18:21 +0000

    20210117

--- 알고리즘/Kth Largest Element in an Array.cpp
+++ 알고리즘/Kth Largest Element in an Array.cpp
@@ -1,22 +1,10 @@
-
-#include <vector>
-#include <queue>
-#include <set>
-
-
-using namespace std;
-
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
-        priority_queue<int> pq; //max heap
-        for(auto ele : nums)  {
-            pq.push(ele);
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; i++) {
+            pq.pop();
         }
-
-        for(int i=0; i<k-1; i++) pq.pop();
-
         return pq.top();
     }
 };
